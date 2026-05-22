96eb637b3cf96b2161d5eff05f3155facf7921ab
Iryna Zahaikevych <izahaikevych@gmail.com>
2026-05-22 16:39:07 +0300

    fix: add api cars request interception

--- cypress/e2e/api_testing.cy.js
+++ cypress/e2e/api_testing.cy.js
@@ -1,4 +1,3 @@
-import { loginAsGuest } from './helpers/LogIn.js';
 import { garagePage } from '../pageObjects/GaragePage.js';
 
 describe('API testing with Cypress', () => {
@@ -8,53 +7,79 @@ describe('API testing with Cypress', () => {
   const carMileage = '50000';
 
   const expenseData = {
-    reportedAt: new Date().toISOString().split('T')[0],
+    reportedAt: '2026-05-22',
     mileage: 50500,
     liters: 40,
     totalCost: 120,
   };
 
   beforeEach(() => {
-    loginAsGuest();
+    cy.visit('/', {
+      auth: {
+        username: 'guest',
+        password: 'welcome2qauto',
+      },
+    });
+
+    const uniqueEmail = `user_${Date.now()}@test.com`;
+
+    cy.request({
+      method: 'POST',
+      url: '/api/auth/signup',
+      body: {
+        name: 'QA',
+        lastName: 'Engineer',
+        email: uniqueEmail,
+        password: 'Password123',
+        repeatPassword: 'Password123',
+      },
+    }).then(() => {
+      cy.contains('button', 'Sign In').click();
+      cy.get('#signinEmail').type(uniqueEmail);
+      cy.get('#signinPassword').type('Password123');
+      cy.get('.modal-footer .btn-primary').click();
+
+      cy.url().should('include', '/panel/garage');
+    });
   });
 
   it('Full cycle: car creation, list validation, expense creation via API and UI check', () => {
+    cy.intercept('POST', '/api/cars').as('createCarRequest');
+
     garagePage.addCarBtn.click();
     garagePage.fillCarForm(carBrand, carModel, carMileage);
     garagePage.submitAddCarBtn.click();
 
-    garagePage.modalContent.should('not.exist');
+    cy.wait('@createCarRequest')
+      .then((interception) => {
+        expect(interception.response.statusCode).to.eq(201);
+        createdCarId = interception.response.body.data.id;
+        expect(createdCarId).to.be.a('number');
+      })
+      .then(() => {
+        garagePage.modalContent.should('not.exist');
+        garagePage.firstCarItem.should('contain.text', carBrand);
 
-    garagePage.firstCarItem.within(() => {
-      cy.get('.car_name').should('have.text', `${carBrand} ${carModel}`);
-      cy.get('input[name="miles"]').invoke('val').should('eq', carMileage);
-    });
+        cy.request('GET', '/api/cars').then((response) => {
+          expect(response.status).to.eq(200);
+          const createdCar = response.body.data.find((car) => car.id === createdCarId);
+          expect(createdCar).to.exist;
+          expect(createdCar.initialMileage).to.eq(Number(carMileage));
+        });
 
-    cy.window().then((win) => {
-      createdCarId = Date.now();
-      expect(createdCarId).to.be.a('number');
-    });
+        cy.createExpenseViaApi(createdCarId, expenseData).then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body.data.carId).to.eq(createdCarId);
+          expect(response.body.data.liters).to.eq(expenseData.liters);
+        });
 
-    garagePage.firstCarItem.should('contain.text', carBrand);
+        garagePage.expensesNavBtn.click();
+        cy.url().should('include', '/panel/expenses');
 
-    cy.createExpenseViaApi(createdCarId, expenseData).then((response) => {
-      expect(response.status).to.exist;
-    });
-
-    garagePage.expensesNavBtn.click();
-    cy.url().should('include', '/panel/expenses');
-
-    cy.get('body').then(($body) => {
-      if ($body.find('.expenses-table').length > 0) {
-        garagePage.expensesTable
-          .find('tbody tr')
-          .first()
-          .within(() => {
-            cy.get('td').eq(1).should('contain.text', expenseData.mileage);
-          });
-      } else {
-        garagePage.mainPanelPage.should('be.visible');
-      }
-    });
+        cy.get('body').should('be.visible');
+        cy.contains(expenseData.mileage).should('be.visible');
+        cy.contains(expenseData.liters).should('be.visible');
+        cy.contains(expenseData.totalCost).should('be.visible');
+      });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,18 @@
 Cypress.Commands.add('createExpenseViaApi', (carId, expenseData) => {
+  const requestBody = {
+    carId: Number(carId),
+    reportedAt: typeof expenseData.reportedAt === 'string' ? expenseData.reportedAt : expenseData.reportedAt[0],
+    mileage: Number(expenseData.mileage),
+    liters: Number(expenseData.liters),
+    totalCost: Number(expenseData.totalCost)
+  };
+
+  cy.log('SENDING EXPENSE BODY:', JSON.stringify(requestBody));
+
   return cy.request({
     method: 'POST',
     url: '/api/expenses',
     failOnStatusCode: false,
-    body: {
-      carId: carId,
-      reportedAt: expenseData.reportedAt,
-      mileage: expenseData.mileage,
-      liters: expenseData.liters,
-      totalCost: expenseData.totalCost,
-    },
+    body: requestBody
   });
 });
